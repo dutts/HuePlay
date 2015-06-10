fddcd40f3e81f60663c75b947bc19ff3cb62e657
Richard <richard@dutton.me.uk>
2015-06-10 18:43:23 +0100

    Colour picker canvas

---
+++ HuePlay/HuePlay.Shared/ColourCanvas.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Windows.ApplicationModel.Store;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Media;
+
+namespace HuePlay
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ColourCanvas : Page
+    {
+        public ColourCanvas()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Grid_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            var offset = e.GetCurrentPoint(this).Position.X / ActualWidth;
+            PickerColour = new SolidColorBrush(grad.GradientStops.GetRelativeColor(offset));
+        }
+
+        public static readonly DependencyProperty PickerColourProperty =
+            DependencyProperty.Register(
+                "PickerColour", typeof (SolidColorBrush), typeof(ColourCanvas), null);
+
+        public SolidColorBrush PickerColour
+        {
+            get { return (SolidColorBrush)GetValue(PickerColourProperty); }
+            set { SetValue(PickerColourProperty, value); }
+        }
+    }
+}
---
+++ HuePlay/HuePlay.Shared/GradientStopCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace HuePlay
+{
+    public static class GradientStopCollectionExtensions
+    {
+        public static Color GetRelativeColor(this GradientStopCollection gsc, double offset)
+        {
+            GradientStop before = gsc.Where(w => w.Offset == gsc.Min(m => m.Offset)).First();
+            GradientStop after = gsc.Where(w => w.Offset == gsc.Max(m => m.Offset)).First();
+
+            foreach (var gs in gsc)
+            {
+                if (gs.Offset < offset && gs.Offset > before.Offset)
+                {
+                    before = gs;
+                }
+                if (gs.Offset > offset && gs.Offset < after.Offset)
+                {
+                    after = gs;
+                }
+            }
+
+            var color = new Color();
+
+            color.A = (byte)((offset - before.Offset) * (after.Color.A - before.Color.A) / (after.Offset - before.Offset) + before.Color.A);
+            color.R = (byte)((offset - before.Offset) * (after.Color.R - before.Color.R) / (after.Offset - before.Offset) + before.Color.R);
+            color.G = (byte)((offset - before.Offset) * (after.Color.G - before.Color.G) / (after.Offset - before.Offset) + before.Color.G);
+            color.B = (byte)((offset - before.Offset) * (after.Color.B - before.Color.B) / (after.Offset - before.Offset) + before.Color.B);
+
+            return color;
+        }
+    }
+}
